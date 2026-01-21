4b7544a320fb8981b2ce33659faba0a5bab3ccfd
AhmedJ8000 <ahmedjamal8000@gmail.com>
2026-01-21 11:35:10 +0300

    Created user table, serializers, and users controller

--- models/user.py
+++ models/user.py
@@ -1,13 +1,17 @@
 from sqlalchemy import Column, Integer, String
 from .base import BaseModel
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class UserModel(BaseModel):
 
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
-
     username = Column(String, unique=True)
-
     email = Column(String, unique=True)
+    password = Column(String, nullable=False)
+
+    def set_password(self, password: str):
+        self.password = pwd_context.hash(password)
