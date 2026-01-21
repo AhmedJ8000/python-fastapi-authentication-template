0930d72691572284501d89226d9974e6c52a91e5
AhmedJ8000 <ahmedjamal8000@gmail.com>
2026-01-21 10:35:33 +0300

    init

---
+++ models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from .base import BaseModel
+
+
+class UserModel(BaseModel):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    username = Column(String, unique=True)
+
+    email = Column(String, unique=True)
